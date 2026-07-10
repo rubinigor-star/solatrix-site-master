27154bb2a8131fa1c9bb6b340e9aecce0af8df50
rubinigor-star <rubin.igor@gmail.com>
2026-07-10 21:37:26 +0300

    Allow public site custom header in lead CORS

--- supabase/functions/submit-lead/index.ts
+++ supabase/functions/submit-lead/index.ts
@@ -2,7 +2,7 @@ import { createClient } from 'npm:@supabase/supabase-js@2';
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, x-solatrix-client, apikey, content-type',
   'Access-Control-Allow-Methods': 'POST, OPTIONS'
 };
 
